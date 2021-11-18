be344c9b6366055990cb4193724f542267c0b1e4
sahedbs23 <ssawon.bs23@gmail.com>
2021-11-18 19:03:37 +0600

    lib updated

--- lib.php
+++ lib.php
@@ -35,16 +35,59 @@ const HTML5_TABLE_NAME = 'html5player';
  */
 function html5player_supports(string $feature): ?bool {
 
-    switch ($feature) {
-        case FEATURE_BACKUP_MOODLE2:
-        case FEATURE_COMPLETION_TRACKS_VIEWS:
-        case FEATURE_SHOW_DESCRIPTION:
-        case FEATURE_MOD_INTRO:
-        case FEATURE_GRADE_HAS_GRADE:
-        return true;
-        default:
-            return null;
+    switch($feature) {
+        case FEATURE_MOD_ARCHETYPE:           return MOD_ARCHETYPE_RESOURCE;
+        case FEATURE_GROUPS:                  return false;
+        case FEATURE_GROUPINGS:               return false;
+        case FEATURE_MOD_INTRO:               return true;
+        case FEATURE_COMPLETION_TRACKS_VIEWS: return true;
+        case FEATURE_GRADE_HAS_GRADE:         return false;
+        case FEATURE_GRADE_OUTCOMES:          return false;
+        case FEATURE_SHOW_DESCRIPTION:        return true;
+
+        default: return null;
     }
+}
+
+/**
+ * This function is used by the reset_course_userdata function in moodlelib.
+ * @param $data the data submitted from the reset course.
+ * @return array status array
+ */
+function html5player_reset_userdata($data) {
+
+    // Any changes to the list of dates that needs to be rolled should be same during course restore and course reset.
+    // See MDL-9367.
+
+    return array();
+}
+
+/**
+ * List the actions that correspond to a view of this module.
+ * This is used by the participation report.
+ *
+ * Note: This is not used by new logging system. Event with
+ *       crud = 'r' and edulevel = LEVEL_PARTICIPATING will
+ *       be considered as view action.
+ *
+ * @return array
+ */
+function html5player_get_view_actions() {
+    return array('view','view all');
+}
+
+/**
+ * List the actions that correspond to a post of this module.
+ * This is used by the participation report.
+ *
+ * Note: This is not used by new logging system. Event with
+ *       crud = ('c' || 'u' || 'd') and edulevel = LEVEL_PARTICIPATING
+ *       will be considered as post action.
+ *
+ * @return array
+ */
+function html5player_get_post_actions() {
+    return array('update', 'add');
 }
 
 /**
@@ -73,7 +116,7 @@ function html5player_update_instance($data, $mform) {
     global $CFG, $DB;
     $data->timemodified = time();
     $data->id           = $data->instance;
-    $DB->update_record('custommod', $data);
+    $DB->update_record('html5player', $data);
     return true;
 }
 
@@ -92,4 +135,433 @@ function html5player_delete_instance($id) {
     $DB->delete_records(HTML5_TABLE_NAME, array('id'=>$html5player->id));
 
     return true;
+}
+
+
+/**
+ * Given a course_module object, this function returns any
+ * "extra" information that may be needed when printing
+ * this activity in a course listing.
+ *
+ * See {@link get_array_of_activities()} in course/lib.php
+ *
+ * @param stdClass $coursemodule
+ * @return cached_cm_info info
+ */
+function html5player_get_coursemodule_info($coursemodule) {
+    global $CFG, $DB;
+    require_once("$CFG->libdir/filelib.php");
+    require_once("$CFG->dirroot/mod/html5player/locallib.php");
+    require_once($CFG->libdir.'/completionlib.php');
+
+    $context = context_module::instance($coursemodule->id);
+
+    if (!$html5player = $DB->get_record('html5player', array('id'=>$coursemodule->instance),
+        'id, name, display, displayoptions, tobemigrated, revision, intro, introformat')) {
+        return NULL;
+    }
+
+    $info = new cached_cm_info();
+    $info->name = $html5player->name;
+    if ($coursemodule->showdescription) {
+        // Convert intro to html. Do not filter cached version, filters run at display time.
+        $info->content = format_module_intro('html5player', $html5player, $coursemodule->id, false);
+    }
+
+    if ($html5player->tobemigrated) {
+        $info->icon ='i/invalid';
+        return $info;
+    }
+
+    // See if there is at least one file.
+    $fs = get_file_storage();
+    $files = $fs->get_area_files($context->id, 'mod_html5player', 'content', 0, 'sortorder DESC, id ASC', false, 0, 0, 1);
+    if (count($files) >= 1) {
+        $mainfile = reset($files);
+        $info->icon = file_file_icon($mainfile, 24);
+        $html5player->mainfile = $mainfile->get_filename();
+    }
+
+    $display = html5player_get_final_display_type($html5player);
+
+    if ($display == RESOURCELIB_DISPLAY_POPUP) {
+        $fullurl = "$CFG->wwwroot/mod/html5player/view.php?id=$coursemodule->id&amp;redirect=1";
+        $options = empty($html5player->displayoptions) ? array() : unserialize($html5player->displayoptions);
+        $width  = empty($options['popupwidth'])  ? 620 : $options['popupwidth'];
+        $height = empty($options['popupheight']) ? 450 : $options['popupheight'];
+        $wh = "width=$width,height=$height,toolbar=no,location=no,menubar=no,copyhistory=no,status=no,directories=no,scrollbars=yes,resizable=yes";
+        $info->onclick = "window.open('$fullurl', '', '$wh'); return false;";
+
+    } else if ($display == RESOURCELIB_DISPLAY_NEW) {
+        $fullurl = "$CFG->wwwroot/mod/html5player/view.php?id=$coursemodule->id&amp;redirect=1";
+        $info->onclick = "window.open('$fullurl'); return false;";
+
+    }
+
+    // If any optional extra details are turned on, store in custom data,
+    // add some file details as well to be used later by html5player_get_optional_details() without retriving.
+    // Do not store filedetails if this is a reference - they will still need to be retrieved every time.
+    if (($filedetails = html5player_get_file_details($html5player, $coursemodule)) && empty($filedetails['isref'])) {
+        $displayoptions = @unserialize($html5player->displayoptions);
+        $displayoptions['filedetails'] = $filedetails;
+        $info->customdata['displayoptions'] = serialize($displayoptions);
+    } else {
+        $info->customdata['displayoptions'] = $html5player->displayoptions;
+    }
+    $info->customdata['display'] = $display;
+
+    return $info;
+}
+
+/**
+ * Called when viewing course page. Shows extra details after the link if
+ * enabled.
+ *
+ * @param cm_info $cm Course module information
+ */
+function html5player_cm_info_view(cm_info $cm) {
+//    global $CFG;
+//    require_once($CFG->dirroot . '/mod/html5player/locallib.php');
+//
+//    $html5player = (object) ['displayoptions' => $cm->customdata['displayoptions']];
+//    $details = html5player_get_optional_details($html5player, $cm);
+//    if ($details) {
+//        $cm->set_after_link(' ' . html_writer::tag('span', $details,
+//                array('class' => 'html5playerlinkdetails')));
+//    }
+}
+
+/**
+ * Lists all browsable file areas
+ *
+ * @package  mod_html5player
+ * @category files
+ * @param stdClass $course course object
+ * @param stdClass $cm course module object
+ * @param stdClass $context context object
+ * @return array
+ */
+function html5player_get_file_areas($course, $cm, $context) {
+    $areas = array();
+    $areas['content'] = get_string('html5playercontent', 'html5player');
+    return $areas;
+}
+
+/**
+ * File browsing support for html5player module content area.
+ *
+ * @package  mod_html5player
+ * @category files
+ * @param stdClass $browser file browser instance
+ * @param stdClass $areas file areas
+ * @param stdClass $course course object
+ * @param stdClass $cm course module object
+ * @param stdClass $context context object
+ * @param string $filearea file area
+ * @param int $itemid item ID
+ * @param string $filepath file path
+ * @param string $filename file name
+ * @return file_info instance or null if not found
+ */
+function html5player_get_file_info($browser, $areas, $course, $cm, $context, $filearea, $itemid, $filepath, $filename) {
+    global $CFG;
+
+    if (!has_capability('moodle/course:managefiles', $context)) {
+        // students can not peak here!
+        return null;
+    }
+
+    $fs = get_file_storage();
+
+    if ($filearea === 'content') {
+        $filepath = is_null($filepath) ? '/' : $filepath;
+        $filename = is_null($filename) ? '.' : $filename;
+
+        $urlbase = $CFG->wwwroot.'/pluginfile.php';
+        if (!$storedfile = $fs->get_file($context->id, 'mod_html5player', 'content', 0, $filepath, $filename)) {
+            if ($filepath === '/' and $filename === '.') {
+                $storedfile = new virtual_root_file($context->id, 'mod_html5player', 'content', 0);
+            } else {
+                // not found
+                return null;
+            }
+        }
+        require_once("$CFG->dirroot/mod/html5player/locallib.php");
+        return new html5player_content_file_info($browser, $context, $storedfile, $urlbase, $areas[$filearea], true, true, true, false);
+    }
+
+    // note: html5player_intro handled in file_browser automatically
+
+    return null;
+}
+
+/**
+ * Serves the html5player files.
+ *
+ * @package  mod_html5player
+ * @category files
+ * @param stdClass $course course object
+ * @param stdClass $cm course module object
+ * @param stdClass $context context object
+ * @param string $filearea file area
+ * @param array $args extra arguments
+ * @param bool $forcedownload whether or not force download
+ * @param array $options additional options affecting the file serving
+ * @return bool false if file not found, does not return if found - just send the file
+ */
+function html5player_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options=array()) {
+    global $CFG, $DB;
+    require_once("$CFG->libdir/resourcelib.php");
+
+    if ($context->contextlevel != CONTEXT_MODULE) {
+        return false;
+    }
+
+    require_course_login($course, true, $cm);
+    if (!has_capability('mod/html5player:view', $context)) {
+        return false;
+    }
+
+    if ($filearea !== 'content') {
+        // intro is handled automatically in pluginfile.php
+        return false;
+    }
+
+    array_shift($args); // ignore revision - designed to prevent caching problems only
+
+    $fs = get_file_storage();
+    $relativepath = implode('/', $args);
+    $fullpath = rtrim("/$context->id/mod_html5player/$filearea/0/$relativepath", '/');
+    do {
+        if (!$file = $fs->get_file_by_hash(sha1($fullpath))) {
+            if ($fs->get_file_by_hash(sha1("$fullpath/."))) {
+                if ($file = $fs->get_file_by_hash(sha1("$fullpath/index.htm"))) {
+                    break;
+                }
+                if ($file = $fs->get_file_by_hash(sha1("$fullpath/index.html"))) {
+                    break;
+                }
+                if ($file = $fs->get_file_by_hash(sha1("$fullpath/Default.htm"))) {
+                    break;
+                }
+            }
+            $html5player = $DB->get_record('html5player', array('id'=>$cm->instance), 'id, legacyfiles', MUST_EXIST);
+            if ($html5player->legacyfiles != RESOURCELIB_LEGACYFILES_ACTIVE) {
+                return false;
+            }
+            if (!$file = resourcelib_try_file_migration('/'.$relativepath, $cm->id, $cm->course, 'mod_html5player', 'content', 0)) {
+                return false;
+            }
+            // file migrate - update flag
+            $html5player->legacyfileslast = time();
+            $DB->update_record('html5player', $html5player);
+        }
+    } while (false);
+
+    // should we apply filters?
+    $mimetype = $file->get_mimetype();
+    if ($mimetype === 'text/html' or $mimetype === 'text/plain' or $mimetype === 'application/xhtml+xml') {
+        $filter = $DB->get_field('html5player', 'filterfiles', array('id'=>$cm->instance));
+        $CFG->embeddedsoforcelinktarget = true;
+    } else {
+        $filter = 0;
+    }
+
+    // finally send the file
+    send_stored_file($file, null, $filter, $forcedownload, $options);
+}
+
+/**
+ * Return a list of page types
+ * @param string $pagetype current page type
+ * @param stdClass $parentcontext Block's parent context
+ * @param stdClass $currentcontext Current context of block
+ */
+function html5player_page_type_list($pagetype, $parentcontext, $currentcontext) {
+    $module_pagetype = array('mod-html5player-*'=>get_string('page-mod-html5player-x', 'html5player'));
+    return $module_pagetype;
+}
+
+/**
+ * Export file html5player contents
+ *
+ * @return array of file content
+ */
+function html5player_export_contents($cm, $baseurl) {
+    global $CFG, $DB;
+    $contents = array();
+    $context = context_module::instance($cm->id);
+    $html5player = $DB->get_record('html5player', array('id'=>$cm->instance), '*', MUST_EXIST);
+
+    $fs = get_file_storage();
+    $files = $fs->get_area_files($context->id, 'mod_html5player', 'content', 0, 'sortorder DESC, id ASC', false);
+
+    foreach ($files as $fileinfo) {
+        $file = array();
+        $file['type'] = 'file';
+        $file['filename']     = $fileinfo->get_filename();
+        $file['filepath']     = $fileinfo->get_filepath();
+        $file['filesize']     = $fileinfo->get_filesize();
+        $file['fileurl']      = file_encode_url("$CFG->wwwroot/" . $baseurl, '/'.$context->id.'/mod_html5player/content/'.$html5player->revision.$fileinfo->get_filepath().$fileinfo->get_filename(), true);
+        $file['timecreated']  = $fileinfo->get_timecreated();
+        $file['timemodified'] = $fileinfo->get_timemodified();
+        $file['sortorder']    = $fileinfo->get_sortorder();
+        $file['userid']       = $fileinfo->get_userid();
+        $file['author']       = $fileinfo->get_author();
+        $file['license']      = $fileinfo->get_license();
+        $file['mimetype']     = $fileinfo->get_mimetype();
+        $file['isexternalfile'] = $fileinfo->is_external_file();
+        if ($file['isexternalfile']) {
+            $file['repositorytype'] = $fileinfo->get_repository_type();
+        }
+        $contents[] = $file;
+    }
+
+    return $contents;
+}
+
+/**
+ * Register the ability to handle drag and drop file uploads
+ * @return array containing details of the files / types the mod can handle
+ */
+function html5player_dndupload_register() {
+    return array('files' => array(
+        array('extension' => '*', 'message' => get_string('dnduploadhtml5player', 'mod_html5player'))
+    ));
+}
+
+/**
+ * Handle a file that has been uploaded
+ * @param object $uploadinfo details of the file / content that has been uploaded
+ * @return int instance id of the newly created mod
+ */
+function html5player_dndupload_handle($uploadinfo) {
+    // Gather the required info.
+    $data = new stdClass();
+    $data->course = $uploadinfo->course->id;
+    $data->name = $uploadinfo->displayname;
+    $data->intro = '<p>'.$uploadinfo->displayname.'</p>';
+    $data->introformat = FORMAT_HTML;
+    if ($uploadinfo->type == 'text/html') {
+        $data->contentformat = FORMAT_HTML;
+        $data->content = clean_param($uploadinfo->content, PARAM_CLEANHTML);
+    } else {
+        $data->contentformat = FORMAT_PLAIN;
+        $data->content = clean_param($uploadinfo->content, PARAM_TEXT);
+    }
+    $data->coursemodule = $uploadinfo->coursemodule;
+
+    // Set the display options to the site defaults.
+    $config = get_config('page');
+    $data->display = $config->display;
+    $data->popupheight = $config->popupheight;
+    $data->popupwidth = $config->popupwidth;
+    $data->printheading = $config->printheading;
+    $data->printintro = $config->printintro;
+    $data->printlastmodified = $config->printlastmodified;
+
+    return html5player_add_instance($data, null);
+}
+/**
+ * Mark the activity completed (if required) and trigger the course_module_viewed event.
+ *
+ * @param  stdClass $html5player   html5player object
+ * @param  stdClass $course     course object
+ * @param  stdClass $cm         course module object
+ * @param  stdClass $context    context object
+ * @since Moodle 3.0
+ */
+function html5player_view($html5player, $course, $cm, $context) {
+
+    // Trigger course_module_viewed event.
+    $params = array(
+        'context' => $context,
+        'objectid' => $html5player->id
+    );
+
+    $event = \mod_html5player\event\course_module_viewed::create($params);
+    $event->add_record_snapshot('course_modules', $cm);
+    $event->add_record_snapshot('course', $course);
+    $event->add_record_snapshot('html5player', $html5player);
+    $event->trigger();
+
+    // Completion.
+    $completion = new completion_info($course);
+    $completion->set_module_viewed($cm);
+}
+
+/**
+ * Check if the module has any update that affects the current user since a given time.
+ *
+ * @param  cm_info $cm course module data
+ * @param  int $from the time to check updates from
+ * @param  array $filter  if we need to check only specific updates
+ * @return stdClass an object with the different type of areas indicating if they were updated or not
+ * @since Moodle 3.2
+ */
+function html5player_check_updates_since(cm_info $cm, $from, $filter = array()) {
+    $updates = course_check_module_updates_since($cm, $from, array('content'), $filter);
+    return $updates;
+}
+
+/**
+ * This function receives a calendar event and returns the action associated with it, or null if there is none.
+ *
+ * This is used by block_myoverview in order to display the event appropriately. If null is returned then the event
+ * is not displayed on the block.
+ *
+ * @param calendar_event $event
+ * @param \core_calendar\action_factory $factory
+ * @return \core_calendar\local\event\entities\action_interface|null
+ */
+function mod_html5player_core_calendar_provide_event_action(calendar_event $event,
+                                                          \core_calendar\action_factory $factory, $userid = 0) {
+
+    global $USER;
+
+    if (empty($userid)) {
+        $userid = $USER->id;
+    }
+
+    $cm = get_fast_modinfo($event->courseid, $userid)->instances['html5player'][$event->instance];
+
+    $completion = new \completion_info($cm->get_course());
+
+    $completiondata = $completion->get_data($cm, false, $userid);
+
+    if ($completiondata->completionstate != COMPLETION_INCOMPLETE) {
+        return null;
+    }
+
+    return $factory->create_instance(
+        get_string('view'),
+        new \moodle_url('/mod/html5player/view.php', ['id' => $cm->id]),
+        1,
+        true
+    );
+}
+
+
+/**
+ * Given an array with a file path, it returns the itemid and the filepath for the defined filearea.
+ *
+ * @param  string $filearea The filearea.
+ * @param  array  $args The path (the part after the filearea and before the filename).
+ * @return array The itemid and the filepath inside the $args path, for the defined filearea.
+ */
+function mod_html5player_get_path_from_pluginfile(string $filearea, array $args) : array {
+    // html5player never has an itemid (the number represents the revision but it's not stored in database).
+    array_shift($args);
+
+    // Get the filepath.
+    if (empty($args)) {
+        $filepath = '/';
+    } else {
+        $filepath = '/' . implode('/', $args) . '/';
+    }
+
+    return [
+        'itemid' => 0,
+        'filepath' => $filepath,
+    ];
 }
